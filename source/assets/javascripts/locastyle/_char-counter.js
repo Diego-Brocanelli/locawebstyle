b25852ceabb876da7b41742d8b5554108327ecf7
Italo Santiago <itu.moraes@gmail.com>
2016-05-25 18:58:18 -0300

    Change the char counter selector from js to jQuery and js code standardization

--- source/assets/javascripts/locastyle/_char-counter.js
+++ source/assets/javascripts/locastyle/_char-counter.js
@@ -7,35 +7,34 @@ locastyle.charCounter = (function() {
     module: '[data-ls-module="charCounter"]'
   };
 
-  
   function checkModule() {
-    var modules = document.querySelectorAll(config.module).length;
+    var modules = $(config.module).length;
     var isModules = (modules > 0) ? true : false;
-    
-    if(isModules) {
-      console.info("Locastyle: module [char counter] successfully initialized.");
+
+    if (isModules) {
+      console.info('Locastyle: module [char counter] successfully initialized.');
     }
 
     return isModules;
   }
 
   function init() {
-    if(checkModule()) {
+    if (checkModule()) {
       countText();
     }
   }
 
   function countText(){
-    $(config.module).each(function(index, field){
+    $(config.module).each(function(index, field) {
       var limit = $(field).attr('maxlength');
       $(field).removeAttr('maxlength').data().maxlength = limit;
-      $(field).after('<p class="ls-help-inline"><small><strong class="ls-char-count ls-number-counter-'+index+'">'+limit+'</strong> caracteres restantes</small></p>');
+      $(field).after('<p class="ls-help-inline"><small><strong class="ls-char-count ls-number-counter-' + index + '">' + limit + '</strong> caracteres restantes</small></p>');
 
-      $(field).keyup(function(){
+      $(field).keyup(function() {
         var count = $(this).val().length;
         var limit = $(this).data().maxlength;
 
-        if(count > limit) {
+        if (count > limit) {
           $(this).val($(this).val().substring(0, limit));
           updateCounter(index, 0);
         } else {
@@ -47,14 +46,13 @@ locastyle.charCounter = (function() {
     });
   }
 
-  function updateCounter(index, count){
-    $('.ls-number-counter-'+index).text(count);
+  function updateCounter(index, count) {
+    $('.ls-number-counter-' + index).text(count);
   }
 
   return {
     init: init
   };
-
 }());
 
 $(document).ready(locastyle.charCounter.init);
