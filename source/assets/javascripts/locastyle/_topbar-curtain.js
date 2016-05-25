41a1cf0ed49a025903b6404f0219d73b9b5ad9e8
Italo Santiago <itu.moraes@gmail.com>
2016-05-25 18:53:20 -0300

    Change the topbar selector from js to jQuery and js code standardization

--- source/assets/javascripts/locastyle/_topbar-curtain.js
+++ source/assets/javascripts/locastyle/_topbar-curtain.js
@@ -8,18 +8,18 @@ locastyle.topbarCurtain = (function() {
   };
 
   function checkModule() {
-    var modules = document.querySelectorAll(config.module).length;
+    var modules = $(config.module).length;
     var isModules = (modules > 0) ? true : false;
-    
-    if(isModules) {
-      console.info("Locastyle: module [topbar curtain] successfully initialized.");
+
+    if (isModules) {
+      console.info('Locastyle: module [topbar curtain] successfully initialized.');
     }
 
     return isModules;
   }
 
   function init() {
-    if(checkModule()) {
+    if (checkModule()) {
       unbind();
       positionTarget();
       bindCloseCurtains();
@@ -31,39 +31,38 @@ locastyle.topbarCurtain = (function() {
   }
 
   function unbind() {
-    $(config.module).off("click.ls, ls.toggleTopCurtain");
-    $(".ls-notification-list").off("click.ls");
-    $("body").off("click.lsTopCurtain");
+    $(config.module).off('click.ls, ls.toggleTopCurtain');
+    $('.ls-notification-list').off('click.ls');
+    $('body').off('click.lsTopCurtain');
   }
 
-  function updateStatusCounter(){
-    $(config.module).each(function(index, element){
+  function updateStatusCounter() {
+    $(config.module).each(function(index, element) {
       var elem = $(element).data('target');
-      var _counter = $(elem+ ' .ls-dismissable:not(.ls-dismissed)').length;
-      if(_counter !== 0) {
-        $('[data-target="'+elem+'"]').attr('data-counter', _counter);
+      var _counter = $(elem + ' .ls-dismissable:not(.ls-dismissed)').length;
+      if (_counter !== 0) {
+        $('[data-target="' + elem + '"]').attr('data-counter', _counter);
       }
-      if(_counter === 0){
-        $('[data-target="'+elem+'"]').removeAttr('data-counter');
+      if (_counter === 0) {
+        $('[data-target="' + elem + '"]').removeAttr('data-counter');
         $(elem + ' .ls-no-notification-message').addClass('active');
       }
     });
   }
 
   function positionTarget() {
-    $(config.module).each(function (index, item){
+    $(config.module).each(function (index, item) {
       var leftDistance = $(item).position().left;
       var iconWidth = (22/ 2);
       var curtainWidth = $($(item).data("target")).width() / 2;
-
       var ua = window.navigator.userAgent;
       var msie = ua.indexOf('MSIE ');
       var trident = ua.indexOf('Trident/');
 
       if (msie > 0 || trident > 0) {
-        $($(item).data("target")).css({left: (leftDistance + iconWidth) - curtainWidth  + "px", top: "60px"});
+        $($(item).data('target')).css({left: (leftDistance + iconWidth) - curtainWidth  + 'px', top: '60px'});
       } else {
-        $($(item).data("target")).css({left: (leftDistance + iconWidth) + curtainWidth  + "px"});
+        $($(item).data('target')).css({left: (leftDistance + iconWidth) + curtainWidth  + 'px'});
       }
 
       bindTopCurtainTrigger(item);
@@ -71,37 +70,38 @@ locastyle.topbarCurtain = (function() {
   }
 
   function bindPreventClosing() {
-    $(".ls-notification-list").on("click.ls", function(evt) {
+    $('.ls-notification-list').on('click.ls', function(evt) {
       evt.stopPropagation();
     });
   }
 
   function bindCloseCurtains() {
-    $("body").on("click.lsTopCurtain", function () {
+    $('body').on('click.lsTopCurtain', function() {
       hideCurtains();
     });
   }
 
   function bindTopCurtainTrigger(trigger) {
-    $(trigger).on("click.ls, ls.toggleTopCurtain", function(evt){
+    $(trigger).on('click.ls, ls.toggleTopCurtain', function(evt) {
       evt.stopPropagation();
-      var targetState = $($(trigger).data("target")).hasClass("ls-active");
+      var targetState = $($(trigger).data('target')).hasClass('ls-active');
       hideCurtains();
-      if(!targetState) {
-        $(trigger).addClass("ls-active");
-        showCurtain($(trigger).data("target"));
+
+      if (!targetState) {
+        $(trigger).addClass('ls-active');
+        showCurtain($(trigger).data('target'));
       }
     });
   }
 
   function showCurtain(curtain) {
-    $(curtain).addClass("ls-active");
+    $(curtain).addClass('ls-active');
     locastyle.dropdown.closeDropdown();
   }
 
   function hideCurtains() {
-    $("[data-ls-module='topbarCurtain']").removeClass("ls-active");
-    $(".ls-notification-list").removeClass("ls-active");
+    $('[data-ls-module="topbarCurtain"]').removeClass('ls-active');
+    $('.ls-notification-list').removeClass('ls-active');
   }
 
   function repositionOnResize() {
@@ -119,7 +119,7 @@ locastyle.topbarCurtain = (function() {
 
   function cloneDropdownToSidebar() {
     var $userAccountTopbar = $('.ls-topbar .ls-user-account');
-    if ( !$('.ls-sidebar .ls-user-account').length ) {
+    if (!$('.ls-sidebar .ls-user-account').length) {
       $userAccountTopbar.clone().prependTo('.ls-sidebar');
     }
   }
@@ -130,7 +130,6 @@ locastyle.topbarCurtain = (function() {
     unbind: unbind,
     updateStatusCounter: updateStatusCounter
   };
-
 }());
 
 $(document).ready(locastyle.topbarCurtain.init);
