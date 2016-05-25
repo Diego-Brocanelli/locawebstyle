d360267731d08bd37f21e1c416c6178489b1e5f7
Italo Santiago <itu.moraes@gmail.com>
2016-05-25 18:55:26 -0300

    Change the progress bar selector from js to jQuery and js code standardization

--- source/assets/javascripts/locastyle/_progress-bar.js
+++ source/assets/javascripts/locastyle/_progress-bar.js
@@ -8,39 +8,38 @@ locastyle.progressBar = (function() {
   }
 
   function checkModule() {
-    var modules = document.querySelectorAll(config.module).length;
+    var modules = $(config.module).length;
     var isModules = (modules > 0) ? true : false;
-    
-    if(isModules) {
-      console.info("Locastyle: module [progressBar] successfully initialized.");
+
+    if (isModules) {
+      console.info('Locastyle: module [progressBar] successfully initialized.');
     }
 
     return isModules;
   }
 
   function init() {
-    if(checkModule()) {
+    if (checkModule()) {
       structureProgressBar();
     }
   }
 
   function structureProgressBar() {
-    $(config.module).each(function (index, element){
-      var percentage = $(element).attr("aria-valuenow");
-      $(element).append("<span aria-valuenow='"+percentage+"%'>");
+    $(config.module).each(function (index, element) {
+      var percentage = $(element).attr('aria-valuenow');
+      $(element).append('<span aria-valuenow="' + percentage + '%">');
       var $bar = $(element).find('span');
       setProgressBarValue($bar, percentage);
     });
   }
 
   function setProgressBarValue(target, percentage) {
-    $(target).css("width", percentage+"%");
+    $(target).css('width', percentage + '%');
   }
 
   return {
     init: init
   };
-
 }());
 
 $(document).ready(locastyle.progressBar.init);
